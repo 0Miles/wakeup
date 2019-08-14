c46a1fdc6dd5a2bf3d1a77de7bdfb13e5c22a233
Miles <swrev5741@gmail.com>
2019-08-15 00:44:31 +0800

    "

--- src/environment-example.ts
+++ src/environment-example.ts
@@ -3,8 +3,8 @@
 export const environment = {
     configs: {
         mac: {
-            macAddress1: 00-00-00-00-00-00",
-            macAddress2: 00-00-00-00-00-00"
+            macAddress1: "00-00-00-00-00-00",
+            macAddress2: "00-00-00-00-00-00"
         },
         address: "192.168.1.255",
         port: "9"
